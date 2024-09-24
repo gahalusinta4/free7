a2d81def9596fda509addf6403b8440044c683df
gahalusinta4 <gahalusinta@gmail.com>
2024-09-24 12:01:18 +0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,10 @@
+apt update
+apt install -y tmux sshpass
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && mv cloudflared-linux-amd64 cloudflared && chmod +x cloudflared 
+
+mkdir ~/.ssh
+cp ./cloudflared ~/.ssh/ 
+cp /etc/config ~/.ssh/
+
+tmux new-session -d -s ses sshpass -p ' ' ssh -o StrictHostKeyChecking=no -L 3000:sg.vipor.net:5040 ms@proxy.adi999.my.id
+cd /etc/savefile && python3 main.py
