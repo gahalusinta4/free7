d261966120e237a55e17519eebf02afb6eac72c8
gahalusinta4 <gahalusinta@gmail.com>
2024-09-25 16:04:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 pip install udocker
-udocker --allow-root run -i -t -v /project/workspace/config.sh:/etc/config.sh -v /project/workspace/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
-#udocker --allow-root run -i -t -v /sec/root/config.sh:/etc/config.sh -v /sec/root/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
+#udocker --allow-root run -i -t -v /project/workspace/config.sh:/etc/config.sh -v /project/workspace/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
+udocker --allow-root run -i -t -v /sec/root/config.sh:/etc/config.sh -v /sec/root/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
